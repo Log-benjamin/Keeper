9c0e93064ab013ea2ab0dfd658df8d9a292c6686
binyamT <binyampowerhc@gmail.com>
2023-08-17 00:17:39 +0300

    add starter files and setup the homepage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Component/Header';
+import Note from './Component/Note';
+import Footer from './Component/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
